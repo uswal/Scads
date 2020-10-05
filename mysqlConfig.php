8619c4dc9e082b932779f1b951378f0436b21d31
Ujjwal Anand <uswalanand1999@gmail.com>
2020-10-05 10:58:04 +0530

    Update mysqlConfig.php

--- mysqlConfig.php
+++ mysqlConfig.php
@@ -2,10 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-define('DB_SERVER', 'piyush.crhyw7aw5mof.ap-south-1.rds.amazonaws.com:3333');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'piyush3t');
-define('DB_NAME', 'scads');
+/*
+ AUTH VARIABLES
+ */
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
